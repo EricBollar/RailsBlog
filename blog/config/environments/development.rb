406a6c56794f0f322dee2e1165d53a0f9e26b2b9
Ubuntu <ubuntu@ip-172-31-34-88.us-east-2.compute.internal>
2021-07-05 20:32:30 +0000

    working online!

--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "ec2-3-16-164-175.us-east-2.compute.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
